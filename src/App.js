7359853ee14d21a144bf79357c58193383afed2e
wwwkang8 <wwwkang8@gmail.com>
2020-06-10 23:10:59 +0900

    Reusable Component with JSX+Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({fav}){
+  console.log({fav});
+  return <h1> I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div className="App" >
       Hello!!
-      <Potato />
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="samgyupsal"/>
+      <Food fav="쭈꾸미"/>
     </div>
   );
 }
