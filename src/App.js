2bf0dd274bb886d5a2ac1a823d27df844d9689f3
wwwkang8 <wwwkang8@gmail.com>
2020-06-16 22:09:33 +0900

    Planning the movie component

--- src/App.js
+++ src/App.js
@@ -26,49 +26,13 @@ function renderFood(dish){
 
 class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    console.log("constructor");
-  }
-
   state = {
-    count: 0
+    isLoading: true
   };
-
-  add = () => {
-    console.log("add");
-    this.setState({count: this.state.count + 1});
-  };
-
-  minus = () => {
-    console.log("minus");
-    this.setState({count: this.state.count-1});
-  };
-
-  componentDidMount(){
-    console.log("component Did mount");
-  }
-
-  componentDidUpdate(){
-    console.log("component Did update");
-  }
-
-  componentWillUnmount(){
-    console.log("good bye world");
-  }
-
-  
 
   render(){
-    console.log("I am rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-      
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 
 }
