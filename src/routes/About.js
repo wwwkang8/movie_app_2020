5ee7e2832f2c7e9f227eedd4b93e4e4fe8e1a5dd
wwwkang8 <wwwkang8@gmail.com>
2020-06-27 14:43:48 +0900

    Building Routers

---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function About(){
+
+    return <span>About this page: I built it. it is cool</span>
+
+}
+
+export default About;
---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component{
+
+  state = {
+    isLoading: true,
+    movie: []
+  };
+
+  getMovies = async() => {
+
+    const {
+      data: {
+        data : {movies}
+      } 
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies, isLoading: false});
+    
+  }
+
+  componentDidMount(){
+
+    this.getMovies();
+
+  }
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return <div>{ isLoading 
+      ? 
+      (<div className ="loader">
+        <span className="loader__text">정호의 영화 리스트 로딩중....</span>
+      </div>)
+      : 
+      <div className = "movies">
+        {movies.map( movie => (
+          <Movie key={movie.id}
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image}
+          genres= {movie.genres} />
+      ))}
+      </div>
+
+    }
+    </div>;
+  }
+
+}
+
+export default Home;
