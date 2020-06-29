9a7903d4fe66ccd281e60559499a950179d9deb2
wwwkang8 <wwwkang8@gmail.com>
2020-06-29 23:22:28 +0900

    Detail function(developing)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,23 +1,55 @@
 import React from "react";
-
+import axios from "axios";
 
 class Detail extends React.Component{
 
+    // 내가 하고 싶은 것은 MovieDetail 데이터를 받아서 화면에 뿌려주고 싶다
+
+    getMovieDetail = async() => {
+
+        const { 
+            data 
+        } = await axios.get(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}&with_images=true&with_cast=true`);
+
+        console.log("this is data part");
+        console.log(data);
+    }
+
     componentDidMount(){
-        console.log(this.props)
-        const { location, history } = this.props;
+
+        const { 
+            location, 
+            history,
+            id } 
+            = this.props;
+
+            console.log(this.props);
         if(location.state == undefined){
             history.push("/");
         }
+
+        this.getMovieDetail(id);
+
     }
 
     render(){
-        const {location} = this.props;
+        const { location } = this.props;
         
         if(location.state){
-            return <span>{location.state.title}</span>
+            return (
+                <div className="detail">
+                    <h1>영화제목이 나올 부분입니다</h1>
+
+                </div>
+            );
+            
+
         }else{
-            return null;
+            return (
+                <div>
+                    <h1>영화 불러오는 중</h1>
+                </div>
+            );
         }
     }
 
